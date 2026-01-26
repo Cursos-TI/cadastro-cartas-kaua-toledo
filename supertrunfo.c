8b90652870898d967eca3d2797d4fd5a88dc8659
Kauã Toledo <toledokaua05@gmail.com>
2026-01-26 18:28:28 -0300

    Utilização de fgets para leitura de strings, ajuste nas variáveis e correções de escrita

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,26 +1,28 @@
 #include <stdio.h> // Inclui a biblioteca padrão de entrada e saída
+#include <stdlib.h>
+#include <string.h>
 
 int main(){
     
-
     // - Declaração de dados da Carta 1
     char estado1;
     char codigo1[5];   //Ex: A01 (3 chars + \0)
-    char cidade1[100];
+    char cidade1[50];
     int populacao1;
     float area1;
     float pib1;
-    int pontos_turisticos1;
+    int pontosturisticos1;
+
 
 
     // - Declaração de dados da Carta 2
     char estado2;
     char codigo2[5];
-    char cidade2[100];
+    char cidade2[50];
     int populacao2;
     float area2;
     float pib2;
-    int pontos_turisticos2;
+    int pontosturisticos2;
 
 
 
@@ -39,9 +41,11 @@ int main(){
     
     printf("Código: ");
     scanf(" %4s", codigo1);  //lê a string do código
+
+    getchar(); //Limpa o buffer
     
     printf("Cidade: ");
-    scanf(" %[^\n]", cidade1);
+    fgets(cidade1, 50, stdin);
     
     printf("População: ");
     scanf(" %d", &populacao1);
@@ -53,7 +57,7 @@ int main(){
     scanf(" %f", &pib1);
     
     printf("Número de Pontos Turísticos: ");
-    scanf(" %d", &pontos_turisticos1);
+    scanf(" %d", &pontosturisticos1);
 
 
     printf("\nCarta 1 cadastrada com sucesso!\n"); //informa o cadastro da primeira carta ao usuário
@@ -66,9 +70,11 @@ int main(){
     
     printf("Código: ");
     scanf(" %4s", codigo2);
+
+    getchar(); //Limpa o buffer
     
     printf("Cidade: ");
-    scanf(" %[^\n]", cidade2);
+    fgets(cidade2, 50, stdin);
     
     printf("População: ");
     scanf(" %d", &populacao2);
@@ -80,7 +86,7 @@ int main(){
     scanf(" %f", &pib2);
     
     printf("Número de Pontos Turísticos: ");
-    scanf(" %d", &pontos_turisticos2);
+    scanf(" %d", &pontosturisticos2);
 
 
     printf("\n===========================================\n");
@@ -96,7 +102,7 @@ int main(){
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f\n", pib1);
-    printf("Pontos Túristicos: %d\n", pontos_turisticos1);
+    printf("Pontos Turísticos: %d\n", pontosturisticos1);
 
 
 
@@ -104,11 +110,11 @@ int main(){
     printf("\n---> CARTA 2 <---\n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
-    printf("Cidade; %s\n", cidade2);
+    printf("Cidade: %s\n", cidade2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f\n", pib2);
-    printf("Pontos Túristicos: %d\n", pontos_turisticos2);
+    printf("Pontos Turísticos: %d\n", pontosturisticos2);
 
     return 0;
 }
